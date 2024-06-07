7c0bb3003dcb3311522a365def1099d0ec188390
Sowmyabala <167969513+Sowmya-21n@users.noreply.github.com>
2024-06-07 15:22:41 +0530

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,36 @@
+package task21;
+
+import java.time.Duration;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Question3 extends MainChrome{
+
+        @Test
+        public void test() throws InterruptedException {
+            //load url
+            chromeDriver.get("https://the-internet.herokuapp.com/iframe");
+
+            //locate frame xpath
+            WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("mce_0_ifr")));
+
+            //locate p tag inside frame
+            WebElement p = chromeDriver.findElement(By.id("tinymce"));
+            Thread.sleep(1000);
+
+            //Send hello people inside p tag using javascript
+            JavascriptExecutor jsc = (JavascriptExecutor) chromeDriver;
+            jsc.executeScript("arguments[0].innerText = 'Hello People';", p);
+
+            //verify the enter name is correct or not
+            String name = p.getText();
+            System.out.println(name);
+
+        }
+    }
+
